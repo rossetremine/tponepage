a97e39daa1cd458994958cc917b3d19c40249efb
Julien ROSSE <julien.rosse456@gmail.com>
2024-04-02 10:38:01 +0200

    Détail pokémon

--- js/utils/PokemonProvider.js
+++ js/utils/PokemonProvider.js
@@ -1,4 +1,4 @@
-import { ENDPOINT } from '../config.js'
+import {ENDPOINT} from '../config.js'
 
 export default class PokemonProvider {
 
@@ -17,7 +17,7 @@ export default class PokemonProvider {
            }
        };
        try {
-           const response = await fetch(`${ENDPOINT}?_limit=${limit}`, options)
+           const response = await fetch(`${ENDPOINT}/pokemons?_limit=${limit}`, options)
            const json = await response.json();
         //    for (let i = 0; i < json.length; i++) {
         //        let pokeApiData = await this.fetchPokeApi(i + 1);
@@ -28,7 +28,37 @@ export default class PokemonProvider {
            console.log('Error getting documents', err)
        }
     }
+    static getPokemonRating = async (id) => {
+        const response = await fetch(`${ENDPOINT}/ratings?id=${id}`)
+        const json = await response.json()
+        if (json.length === 0) {
+            return { sum: 0, count: 0 }
+        }
+        return json[0]
+    }
 
+    static addPokemonRating = async (id, sum, count) => {
+        const options = {
+            method: (count === 1 ? 'POST' : 'PUT'),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: id,
+                sum: sum,
+                count: count
+            })
+        }
+        let response
+        if (count === 1) {
+            delete options.body.id
+            response = await fetch(`${ENDPOINT}/ratings`, options)
+        }
+        else {
+            response = await fetch(`${ENDPOINT}/ratings/${id}`, options)
+        }
+        return await response.json()
+    }
     static getPokemon = async (id) => {
         const options = {
            method: 'GET',
@@ -37,7 +67,7 @@ export default class PokemonProvider {
            }
        };
        try {
-           const response = await fetch(`${ENDPOINT}/` + id, options)
+           const response = await fetch(`${ENDPOINT}/pokemons/` + id, options)
            const json = await response.json();
 
             // let pokeApiData = await this.fetchPokeApi(id);
--- js/views/pages/PokemonDetail.js
+++ js/views/pages/PokemonDetail.js
@@ -2,26 +2,51 @@ import Utils from "../../utils/Utils.js";
 import PokemonProvider from "../../utils/PokemonProvider.js";
 
 export default class PokemonDetail {
+    pokemon = null
+    ratingData = null
+    rate = async (event) => {
+        event.preventDefault()
+        const rating = document.getElementById('rating').value
+        await PokemonProvider.addPokemonRating(this.pokemon.id, this.ratingData.sum + parseInt(rating), this.ratingData.count + 1)
+        location.reload()
+    }
     async render () {
-        const pokemon = await PokemonProvider.getPokemon(Utils.parseRequestURL().id)
-        console.log(pokemon)
+        this.pokemon = await PokemonProvider.getPokemon(Utils.parseRequestURL().id)
+        this.ratingData = await PokemonProvider.getPokemonRating(this.pokemon.id)
         let view =  `
-            <h2>${pokemon.name.french}</h2>
-            Type principal: ${pokemon.type[0]}
-            ${pokemon.type[1] ? `<br>Type secondaire: ${pokemon.type[1]}` : ''}
+            <h2>${this.pokemon.name.french}</h2>
+            <img src="${this.pokemon.pokeApiData.sprites.front_default}" alt="${this.pokemon.name.french}">
+            <br>
+            Type principal: ${this.pokemon.type[0]}
+            ${this.pokemon.type[1] ? `<br>Type secondaire: ${this.pokemon.type[1]}` : ''}
             
             <ul>
-                <li>HP: ${pokemon.base.HP}</li>
-                <li>Attack: ${pokemon.base.Attack}</li>
-                <li>Defense: ${pokemon.base.Defense}</li>
-                <li>Sp. Attack: ${pokemon.base['Sp. Attack']}</li>
-                <li>Sp. Defense: ${pokemon.base['Sp. Defense']}</li>
-                <li>Speed: ${pokemon.base.Speed}</li>
-</ul>
+                <li>HP: ${this.pokemon.base.HP}</li>
+                <li>Attack: ${this.pokemon.base.Attack}</li>
+                <li>Defense: ${this.pokemon.base.Defense}</li>
+                <li>Sp. Attack: ${this.pokemon.base['Sp. Attack']}</li>
+                <li>Sp. Defense: ${this.pokemon.base['Sp. Defense']}</li>
+                <li>Speed: ${this.pokemon.base.Speed}</li>
+            </ul>
+            <h3>Rating</h3>
+            <p>Rating: ${
+                this.ratingData &&
+                this.ratingData.count > 0
+                ? (this.ratingData.sum / this.ratingData.count).toFixed(2) + ' / 5 (' + this.ratingData.count + ' votes)'
+                : 'No rating yet'
+        }</p>
+            <form id="ratingForm"
+                <label for="rating">Rating</label>
+                <input type="number" id="rating" name="rating" min="1" max="5">
+                <button type="submit">Submit</button>
+            </form>
         `;
         return view
     }
 
     async afterRender() {
+    }
+    async afterRender () {
+        document.getElementById('ratingForm').addEventListener('submit', this.rate)
     }
 }
